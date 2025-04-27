490afa518b91ebd76583bd0712d822659ad359cb
Charles <dilfanianc@gmail.com>
2025-04-27 16:12:30 -0400

    Commit
    in

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('myButton').addEventListener('click', function() {
+  window.open('https://www.linkedin.com/in/charles-dilfanian', '_blank'); // Open in a new tab
+});
+document.getElementById('myButton2').addEventListener('click', function() {
+    window.open('https://github.com/CharlesDilf', '_blank'); // Open in a new tab
+  });
+document.getElementById('myButton3').addEventListener('click', function() {
+    window.open('https://leetcode.com/u/charlesdilf/', '_blank'); // Open in a new tab
+  });
+document.getElementById('myButton5').addEventListener('click', function() {
+    window.open('https://www.instagram.com/charles_dilf/', '_blank'); // Open in a new tab
+  });
+
+  document.getElementById('myButton4').addEventListener('click', function() {
+    window.location.href = 'SOA.html'; // Redirect to SOA.html
+});
